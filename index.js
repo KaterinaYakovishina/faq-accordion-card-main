aea9c4e4d7e3c0155164b35c68a9e5e47b103bfc
Katerina Y <k.yakovishina@gmail.com>
2023-10-18 21:23:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const answers = document.querySelectorAll(".answer-block");
+const questions = document.querySelectorAll(".question");
+const arrows = document.querySelectorAll("#arrowIcon");
+let currentQuestion;
+let currentAnswer;
+//answers collapsed
+answers.forEach((answer) => {
+  answer.style.display = "none";
+});
+//guestion id = answer id
+questions.forEach((question, index) => {
+  question.setAttribute("id", index);
+  question.addEventListener("click", showAnswer);
+});
+
+//toggle
+function showAnswer(question) {
+  currentQuestion = question.target.id;
+  currentAnswer = answers[currentQuestion];
+
+  if (currentAnswer.style.display === "none") {
+    arrows[currentQuestion].style.transform = "rotate(180deg)";
+    questions[currentQuestion].style.color = "var(--tx-pr-blue)";
+    questions[currentQuestion].style.fontWeight = "700";
+    currentAnswer.style.display = "initial";
+  } else {
+    arrows[currentQuestion].removeAttribute('style');
+    questions[currentQuestion].removeAttribute('style');
+    currentAnswer.style.display = "none";
+}
+}
