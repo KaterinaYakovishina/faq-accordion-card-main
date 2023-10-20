b9af8395f293a2696d9b07e9fc8adec6e6107b22
Katerina Y <k.yakovishina@gmail.com>
2023-10-20 22:42:32 +0300

    Rev 2.2

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const questions = document.querySelectorAll(".question");
 const arrows = document.querySelectorAll("#arrowIcon");
 let currentQuestionId;
 let currentAnswer;
+let currentQuestion;
+let currentArrow;
 let numberOfOpenQuestions = 0;
 let openAswersList = [];
 let openQuestionsList = [];
+let transformedArrows = [];
 
 
 answers.forEach((answer, index) => {
@@ -13,37 +16,47 @@ answers.forEach((answer, index) => {
   answer.setAttribute('id', index);
 });
 
+
 questions.forEach((question, index) => {
   question.setAttribute("id", index);
   question.addEventListener("click", showAnswer);
 });
 
+
+
 function showAnswer(question) {
   currentQuestionId = question.target.id;
   currentAnswer = answers[currentQuestionId];
+  currentQuestion = questions[currentQuestionId];
+  currentArrow = arrows[currentQuestionId];
 
-    
+   
   if (currentAnswer.style.display === "none") {
      
-    arrows[currentQuestionId].style.transform = "rotate(180deg)";
-    questions[currentQuestionId].style.color = "var(--tx-pr-blue)";
-    questions[currentQuestionId].style.fontWeight = "700";
+    currentArrow.style.transform = "rotate(180deg)";
+    currentQuestion.style.color = "var(--tx-pr-blue)";
+    currentQuestion.style.fontWeight = "700";
     currentAnswer.style.display = "initial";
     openAswersList.push(currentAnswer);
-    openQuestionsList.push(questions[currentQuestionId]);
+    openQuestionsList.push(currentQuestion);
+    transformedArrows.push(currentArrow);
     numberOfOpenQuestions += 1;
        
   } 
-
- if (numberOfOpenQuestions === 2) {
+  
+  if (numberOfOpenQuestions === 2) {
+    transformedArrows[0].removeAttribute('style');
     openAswersList[0].removeAttribute('style');
     openQuestionsList[0].removeAttribute('style');
     openAswersList[0].style.display = "none";
     numberOfOpenQuestions -= 1;
     openAswersList.shift(openAswersList[0]);   
     openQuestionsList.shift(openQuestionsList[0]);
+    transformedArrows.shift(transformedArrows[0]);
   }
  
+console.log(transformedArrows)
+    
   }
 
  
